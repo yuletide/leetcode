504d5d5da4fcfa695ea1f71603f65776e50b1b35
Alex Yule <adyule@gmail.com>
2023-01-10 15:36:02 -0800

    Time: 73 ms (97.30%), Space: 44 MB (58.78%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -1,5 +1,34 @@
-from collections import Counter
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        if s == t: return True
-        return Counter(s) == Counter(t)
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    let sHash = {}
+    let tHash = {}
+    
+    if (s==t){
+        return true
+    }
+    
+    if (s.length !== t.length){
+        return false
+    }
+    
+    // build some hashes
+    s.split("").forEach(c=>{
+        sHash[c] = sHash[c] ? sHash[c]+1 : 1
+    })
+    t.split("").forEach(c=>{
+        tHash[c] = tHash[c] ? tHash[c]+1 : 1
+    })
+    
+    console.log(sHash, tHash)
+    // theres probably a nicer way to check hash equality
+    for (key of Object.keys(sHash)){
+        if (sHash[key] !== tHash[key]){
+            return false
+        }
+    }
+    return true
+};
