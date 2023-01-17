0849d06f055435189cd96603ff93b43f49a4cd26
Alex Yule <adyule@gmail.com>
2023-01-17 10:46:31 -0800

    Time: 22 ms (99.02%), Space: 13.8 MB (96.28%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -2,9 +2,16 @@ from functools import cache;
 class Solution:
     @cache
     def climbStairs(self, n: int) -> int:
-        if n==1:
-            return 1
-        elif n==2:
-            return 2
-        elif n>2:
-            return self.climbStairs(n-1) + self.climbStairs(n-2)
+        # if n==1:
+        #     return 1
+        # elif n==2:
+        #     return 2
+        # elif n>2:
+        #     return self.climbStairs(n-1) + self.climbStairs(n-2)
+    
+        dp = [0, 1, 2]
+        for i in range (3,n+1):
+            dp.append(dp[i-1] + dp[i-2])
+
+        return dp[n]
+    
