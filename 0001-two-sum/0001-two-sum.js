d7398e209b7ee05b8f59d1133d4e1927d18569fb
Alex Yule <adyule@gmail.com>
2023-01-24 21:59:14 -0800

    Time: 72 ms (84.87%), Space: 44.9 MB (7.64%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,15 +3,30 @@
  * @param {number} target
  * @return {number[]}
  */
+
+// Faster O(n) Solution
 var twoSum = function(nums, target) {
-  // brute force:
-  let solution = []
+  let hash = new Map();
   for (const [i, num] of nums.entries()){
-      //console.log(num,i)
+    //   console.log(num,i);
       const complement = target-num;
-      //console.log("complement", complement)
-      const complementIndex = nums.indexOf(complement, i+1);
-      //console.log('complement index', complementIndex);
-      if (complementIndex > -1) return [i, complementIndex];
+    //   console.log("complement", complement);
+      if (hash.has(complement)){
+          return [hash.get(complement), i]
+      }
+    hash.set(num, i);
   }
 };
+
+// First attempt, slower solution because of indexOf making it closer to O(n2)
+// var twoSum = function(nums, target) {
+//   let solution = []
+//   for (const [i, num] of nums.entries()){
+//       //console.log(num,i)
+//       const complement = target-num;
+//       //console.log("complement", complement)
+//       const complementIndex = nums.indexOf(complement, i+1);
+//       //console.log('complement index', complementIndex);
+//       if (complementIndex > -1) return [i, complementIndex];
+//   }
+// };
