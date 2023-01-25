3e43937d7500fb98bf529cd94d741acaf18365b0
Alex Yule <adyule@gmail.com>
2023-01-24 16:36:00 -0800

    Time: 151 ms (35.40%), Space: 17.7 MB (70.43%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,8 +2,9 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         hash = {}
         for word in strs:
-            wordCounter = Counter(word)
-            key = json.dumps(sorted(wordCounter.items()))
+            # wordCounter = Counter(word)
+            # key = json.dumps(sorted(wordCounter.items()))
+            key = "".join(sorted(word))
             #print(key)
             if key in hash.keys():
                 hash[key].append(word)
